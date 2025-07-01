6643facdca5bfde2c64e785a96a43cad6a7a3fa8
Akshat Pandey <akshatpandey26.2004@gmail.com>
2025-07-01 23:49:31 +0530

    day

---
+++ Trees/Binary Tree/Binarytree.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Binarytree {
+    static class Node{
+        int val;
+        Node left,right;
+
+        Node(int val){
+            this.val=val;
+            this.left=null;
+            this.right=null;
+        }
+        
+
+
+
+
+    }
+}
